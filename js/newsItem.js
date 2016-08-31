5eb1ee75f9beb5105bc79f62cd6ade5fd1cbed53
Blake Netzeband <blake.netzeband@gmail.com>
2016-08-31 11:28:44 -0600

    added subtext for each news item

--- js/newsItem.js
+++ js/newsItem.js
@@ -1,14 +1,49 @@
 const $ = require('jquery');
 const React = require('react');
+const moment = require('moment');
+
+const url = require('url')
 
 const NewsItem = React.createClass({
+  getDomain: function () {
+    return url.parse(this.props.item.url).hostname;
+  },
+  getCommentLink: function () {
+    let commentText = 'discuss';
+    if (this.props.item.kids && this.props.item.kids.length) {
+      commentText = this.props.item.kids.length + ' comments';
+    }
+    return (
+      <a href={'https://news.ycombinator.com/item?id=' + this.props.item.id}>{commentText}</a>
+    );
+  },
+  getTitle: function () {
+    return (
+      <div>
+        <a className="news-item-title"href={this.props.item.url}>{this.props.item.title}</a>
+        <span className="news-item-domain">
+          ({this.getDomain()})
+        </span>
+      </div>
+    );
+  },
+  getSubText: function () {
+    return (
+      <div className='news-item-subtext'>
+        {this.props.item.score}
+        points by <a href={'https://news.ycombinator.com/user?id=' + this.props.item.by}>{this.props.item.by}</a>
+        {moment.utc(this.props.item.time * 1000).fromNow()} | {this.getCommentLink()}
+      </div>
+    )
+  },
   render: function () {
     return (
       <div className='news-item'>
-        <a className='news-item-title' href='{this.props.item.url}'>{this.props.item.title}</a>
+        {this.getTitle()}
+        {this.getSubText()}
       </div>
     );
-  }
+  },
 })
 
 module.exports = NewsItem;
--- js/newsItemTest.js
+++ js/newsItemTest.js
@@ -1,10 +1,12 @@
 const $ = require('jquery');
 const React = require('react');
+const ReactDOM = require('react-dom');
+
 const NewsItem = require('./newsItem');
 
 $.ajax({
   url: '/json/data.json'
 }).then(function (items) {
   console.log('items: ', items);
-  React.render(<NewsItem item={items[0]} rank={1} />, $('#content')[0]);
+  ReactDOM.render(<NewsItem item={items[0]} rank={1} />, $('#content')[0]);
 });
